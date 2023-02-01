8669741cdd8cfdb131683b959bd6a7a361cf9412
jiyoon <vywns9978@gmail.com>
2023-02-01 17:05:43 +0900

    feat: 이벤트 배너 swiper 작업 중 에러

---
+++ client/js/mainpage.js
@@ -0,0 +1,18 @@
+
+
+
+const swiper = new Swiper('.swiper', {
+  autoplay:true,
+  loop:true,
+  slidesPerView:'auto',
+  // speed:2000,
+  // parallax: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   type: "fraction",
+  //   },
+  // navigation: {
+  //   nextEl: ".event-button-next",
+  //   prevEL: ".event-button-prev",
+  // },
+});
