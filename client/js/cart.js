d5e9021e89ba06c1c125e7ec983715fe88e7904c
Yujin <jyj15938@gmail.com>
2023-02-02 22:25:45 +0900

    cart.js click 이벤트wip

--- client/js/cart.js
+++ client/js/cart.js
@@ -1,5 +1,16 @@
-const checkedFalse = document.querySelector('#check-false');
+import { getNode, getNodes } from '../lib/dom/getNode.js';
 
-const toggleCheckImg = checkedFalse.addEventListener('click', () => {
-  document.querySelector('img').setAttribute('src', './assets/icon/isChecked=true.png');
-});
+const checkedFalse = getNodes('.is-checked-false');
+
+function changeCheckImg() {
+  for (let i = 0; i < checkedFalse.length; i++) {
+    checkedFalse[i].addEventListener('click', checked);
+  }
+}
+function checked() {
+  if (checkedFalse.src.match('./assets/icon/isChecked=false.png')) {
+    checkedFalse.src = './assets/icon/isChecked=true.png';
+  } else {
+    checkedFalse.src = './assets/icon/isChecked=false.png';
+  }
+}
