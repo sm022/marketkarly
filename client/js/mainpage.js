b566c2f1a798a1930bf8a1e9264c5c775e28b3b4
jiyoon <vywns9978@gmail.com>
2023-02-02 23:52:58 +0900

    feat:상품 리스트 스와이퍼 구현

--- client/js/mainpage.js
+++ client/js/mainpage.js
@@ -1,7 +1,5 @@
 
-
-
-const swiper = new Swiper('.swiper-E', {
+const swiper_E = new Swiper('.swiper-E', {
   autoplay:true,
   loop:true,
   slidesPerView:'auto',
@@ -14,5 +12,30 @@ const swiper = new Swiper('.swiper-E', {
   navigation: {
     nextEl: '.event-button-next',
     prevEl: '.event-button-prev',
+  },
+});
+
+
+const swiper_R = new Swiper('.swiper-R', {
+  loopFillGroupWithBlank: 'auto',
+  spaceBetween: 18,
+  loop:true,
+  speed:1000,
+  parallax: true,
+  navigation: {
+    nextEl: '.recommend-next-button',
+    prevEl: '.recommend-prev-button',
+  },
+});
+
+const swiper_S = new Swiper('.swiper-S', {
+  loopFillGroupWithBlank: 'auto',
+  spaceBetween: 18,
+  loop:true,
+  speed:1000,
+  parallax: true,
+  navigation: {
+    nextEl: '.sale-next-button',
+    prevEl: '.sale-prev-button',
   },
 });
