5d419d364df338ab98fe86d047060200b41a3d03
jiyoon <vywns9978@gmail.com>
2023-02-02 13:17:17 +0900

    fix:스와이퍼 에러수정 style:이벤트 스와이퍼 완성

--- client/js/mainpage.js
+++ client/js/mainpage.js
@@ -1,18 +1,18 @@
 
 
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-E', {
   autoplay:true,
   loop:true,
   slidesPerView:'auto',
-  // speed:2000,
-  // parallax: true,
-  // pagination: {
-  //   el: ".swiper-pagination",
-  //   type: "fraction",
-  //   },
-  // navigation: {
-  //   nextEl: ".event-button-next",
-  //   prevEL: ".event-button-prev",
-  // },
+  speed:2000,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    },
+  navigation: {
+    nextEl: '.event-button-next',
+    prevEl: '.event-button-prev',
+  },
 });
