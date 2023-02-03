2bdd6ea42dc0d4f1fb1cb173c9531db205a1a6e7
Yujin <jyj15938@gmail.com>
2023-02-03 10:08:54 +0900

    [feat] carlist에 api 받아와서 데이터클래스로 만들고 뿌려줌

--- client/js/cart.js
+++ client/js/cart.js
@@ -1,16 +1,53 @@
-import { getNode, getNodes } from '../lib/dom/getNode.js';
+import { xhrPromise } from '/client/lib/utils/xhr.js';
+import CartItemDataClass from '/client/lib/data-class/cart-item.data-class.js';
 
-const checkedFalse = getNodes('.is-checked-false');
+import { getNode } from '/client/lib/dom/getNode.js';
 
-function changeCheckImg() {
-  for (let i = 0; i < checkedFalse.length; i++) {
-    checkedFalse[i].addEventListener('click', checked);
+class CartProcessClass {
+  coldList = getNode('.cold-list').querySelector('.main-list');
+  frozenList = getNode('.frozen-list').querySelector('.main-list');
+  normalList = getNode('.normal-list').querySelector('.main-list');
+
+  async loadFoodsFromApi() {
+    const url = 'http://localhost:3000/products';
+    return xhrPromise({ url }).catch((err) => {
+      console.log(err);
+      return [];
+    });
+  }
+
+  async loadFoods() {
+    const foods = await this.loadFoodsFromApi();
+    if (foods.length < 1) {
+      alert('아이템을 가져오지 못했습니다. 서버 상태를 확인해주세요.');
+    }
+
+    return foods.map((food) => new CartItemDataClass(food));
+  }
+
+  async run() {
+    const foods = await this.loadFoods();
+
+    foods.map((food) => {
+      switch (food.type) {
+        case 'COLD':
+          this.coldList.appendChild(food.toElement());
+          break;
+
+        case 'FROZEN':
+          this.frozenList.appendChild(food.toElement());
+          break;
+
+        case 'NORMAL':
+          this.normalList.appendChild(food.toElement());
+          break;
+        default:
+          alert('올바르지 않은 타입의 식품입니다.', food.name);
+          return;
+      }
+    });
   }
 }
-function checked() {
-  if (checkedFalse.src.match('./assets/icon/isChecked=false.png')) {
-    checkedFalse.src = './assets/icon/isChecked=true.png';
-  } else {
-    checkedFalse.src = './assets/icon/isChecked=false.png';
-  }
-}
+
+const cartProcessClass = new CartProcessClass();
+await cartProcessClass.run();
