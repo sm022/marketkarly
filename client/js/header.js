2972a2b5c8c736cde26000fc335f315c57bfe062
somin <smhs12@naver.com>
2023-02-05 16:37:52 +0900

    feat: 헤더 스크롤 이벤트 기능

---
+++ client/js/header.js
@@ -0,0 +1,30 @@
+import { getNode } from "../lib/dom/getNode.js";
+import { addClass, css, removeClass } from "../lib/dom/css.js";
+
+// 헤더
+const normalHeader = getNode(".normal-header");
+const scrollHeader = getNode(".scroll-header");
+const main = getNode("main");
+
+// 링크마다 해당하는 부분에 도달하면 is-selected 클래스 추가
+// 이전 링크는 is-selected 제거
+// footer 위에 도달하면 마지막 위치에 있다가 스크롤 올릴 때 고정되어 나타남
+// 쓰로틀, 디바운스 추가해보기
+const onScrollHandler = () => {
+  let windowTop = window.scrollY;
+  // console.log(windowTop);
+  const normalClassList = normalHeader.classList;
+
+  if (windowTop > 160 && !normalClassList.contains("close")) {
+    addClass(normalHeader, "close");
+    removeClass(scrollHeader, "close");
+    css(main, "padding-top", "240px");
+  }
+  if (windowTop === 0 && normalClassList.contains("close")) {
+    addClass(scrollHeader, "close");
+    removeClass(normalHeader, "close");
+    css(main, "padding-top", "0");
+  }
+};
+
+window.addEventListener("scroll", onScrollHandler);
