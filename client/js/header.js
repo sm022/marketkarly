6d16a1fb7a858a67ad7cc737288c61841cc4240f
somin <smhs12@naver.com>
2023-02-05 16:44:55 +0900

    feat: 카테고리 마우스오버 이벤트 기능

--- client/js/header.js
+++ client/js/header.js
@@ -6,6 +6,15 @@ const normalHeader = getNode(".normal-header");
 const scrollHeader = getNode(".scroll-header");
 const main = getNode("main");
 
+// 카테고리 메뉴
+const categoryMenu = getNode(".category-menu");
+const categoryList = getNode(".category-list");
+
+const onMouseoverHandler = () => {
+  addClass(categoryList, "is-active");
+};
+
+categoryMenu.addEventListener("mouseover", onMouseoverHandler);
 // 링크마다 해당하는 부분에 도달하면 is-selected 클래스 추가
 // 이전 링크는 is-selected 제거
 // footer 위에 도달하면 마지막 위치에 있다가 스크롤 올릴 때 고정되어 나타남
