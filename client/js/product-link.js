af6612a1db43f3ce88d214f30318d0197b5c9028
somin <smhs12@naver.com>
2023-02-08 18:56:00 +0900

    feat: 상품상세페이지 링크 상단 고정 기능

--- client/js/product-link.js
+++ client/js/product-link.js
@@ -7,27 +7,42 @@ const hyperLinks = getNodes(".hyper-link-container a");
 
 const onScrollHandler = () => {
   let windowTop = window.scrollY;
-
+  console.log(windowTop);
   if (windowTop >= 1230 && !containClass(linkContainer, "is-fixed")) {
     addClass(linkContainer, "is-fixed");
-    addClass(hyperLinks[0], "is-selected");
     css(productDescription, "padding-top", "80px");
   }
-  // 상세정보 -> 상품설명
-  if (windowTop < 2800 && containClass(hyperLinks[1], "is-selected")) {
+  if (windowTop >= 1230 && windowTop < 2830) {
     addClass(hyperLinks[0], "is-selected");
-    removeClass(hyperLinks[1], "is-selected");
   }
-  //상세정보 도달
-  if (windowTop >= 2800 && !containClass(hyperLinks[1], "is-selected")) {
-    addClass(hyperLinks[1], "is-selected");
+  if (!(windowTop >= 1230 && windowTop < 2830)) {
     removeClass(hyperLinks[0], "is-selected");
   }
-  // 링크 고정 해제
+  if (windowTop >= 2830 && windowTop < 4860) {
+    addClass(hyperLinks[1], "is-selected");
+  }
+  if (!(windowTop >= 2830 && windowTop < 4860)) {
+    removeClass(hyperLinks[1], "is-selected");
+  }
+  if (windowTop >= 4860 && windowTop < 6550) {
+    addClass(hyperLinks[2], "is-selected");
+  }
+  if (!(windowTop >= 4860 && windowTop < 6550)) {
+    removeClass(hyperLinks[2], "is-selected");
+  }
+  if (windowTop >= 6550) {
+    addClass(hyperLinks[3], "is-selected");
+  }
+  if (!(windowTop >= 6550)) {
+    removeClass(hyperLinks[3], "is-selected");
+  }
+
   if (windowTop <= 1180 && containClass(linkContainer, "is-fixed")) {
     removeClass(linkContainer, "is-fixed");
     css(productDescription, "padding-top", "0");
     removeClass(hyperLinks[0], "is-selected");
+    removeClass(hyperLinks[1], "is-selected");
+    removeClass(hyperLinks[2], "is-selected");
   }
 };
 window.addEventListener("scroll", onScrollHandler);
